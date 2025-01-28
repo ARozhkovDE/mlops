96ee3d0cdb55e7d4beef198f15ef98f8fd5f333d
Alorozhkov <alorozhkov@compute-vm-2-2-20-ssd-1737798427544.ru-central1.internal>
2025-01-28 00:20:41 +0000

    add hdfs cluster

--- homework/01_terraform/main.tf
+++ homework/01_terraform/main.tf
@@ -1,20 +1,12 @@
 terraform {
-    required_providers {
-        yandex = {
-            source = "yandex-cloud/yandex"
-        }
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
     }
-    required_version = ">= 0.13"
+  }
+  required_version = ">= 0.13"
 }
-
-locals {
-  folder_id = "dn292ldmt4c9p0m5gaq7"
-  cloud_id = "b1gd7trl7o9eihi7jvr1"
-}
-
 
 provider "yandex" {
-cloud_id = locals.cloud_id
-folder_id = locals.folder_id
-zone = "ru-central1-d" # зона доступности по умолчанию
+  service_account_key_file = "key.json"
 }
---
+++ homework/01_terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "folder_id" {
+    description = "folder id"
+    type = string
+}
+
+variable "cloud_id" {
+    description = "cloud_id"
+    type = string
+}
+ 
+variable "default_zone"{
+    description = "default zone"
+    type = string
+    default = "ru-central1-d"
+}
